6db02dcba4f438cb82fd871ed7e8cb4ba3bfe85c
smchow <sunita.m.chowdhury@gmail.com>
2017-03-23 14:45:52 -0400

    changes for heroku, GET route is working

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL){
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -8,6 +11,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+var burger_routes = require("./controllers/burgers_controller.js");
 
-app.use("/burgers", routes);
+app.use("/burgers", burger_routes);
 
-app.listen(port);
+app.listen(port, function(){
+	console.log('listening on port ' + port)
+});
